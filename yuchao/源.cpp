a26fae1031a19b27d0bfa11beb18d5edc832b01b
Tang102107 <2955133891@qq.com>
2024-01-31 19:39:46 +0800

    提交代码

---
+++ yuchao/源.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+	//将大写字母转化为小写字母
+	//char c1, c2;
+	//c1 = 'A';
+	//c2 = c1 + 32;
+	//printf("%c\n",c2);
+	//printf("%d\n",c2);
+	//return 0;
+	//输入三个数a,b,c要求按从大到小的顺序输出
+	/*float a, b, c, t;
+	scanf_s("%f,%f,%f", &a, &b, &c);
+	if (a > b)
+	{t = a;
+	a = b;
+	b = t;
+	}
+	if (a > c)
+	{t = a;
+	a = c;
+	c = t;
+	}
+	if (b > c)
+	{
+		t = b;
+		b = c;
+		c = t;
+	}
+printf("%5.2f,%5.2f,%5.2f\n", a, b, c);
+return 0;*/
+//}
+// 	   Switch语句
+//	char grade;
+//	scanf_s("%c", &grade);
+//	printf("Your score:");
+//	switch (grade)
+//	{
+//	case'A':printf("85~100\n"); break;
+//	case'B':printf("70~84\n"); break;
+//	case'C':printf("60~69\n"); break;
+//	case'D':printf("<60\n"); break;
+//	default:printf("emter date error!\n");
+//	}
+//	return 0;
+//}
