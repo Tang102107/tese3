3bf655cc57897eaed9cbd39265dd1a9db369587f
Tang102107 <2955133891@qq.com>
2024-02-01 19:00:22 +0800

    提交

---
+++ 2.1/2.1/源.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (; i < 10; i++)
+//	{
+//		for (; j < 10; j++)
+//		{
+//			printf("hehe\n");
+//		}
+//	}
+//	return 0;
+//}	
+
+//{int x, y;
+//for (x = 0, y = 0; x < 2 && y < 5; ++x, ++y)
+//{
+//	printf("hi\n");
+//}
+//return 0;
+//}
+//{
+//	int i = 0;
+//	int k = 0;
+//	for (i = 0, k = 0; k = 0; i++, k++)
+//		k++;
+//	return 0;
+//}
+
+//{
+//	int i = 1;
+//	do
+//	{
+//		if (i == 5)
+//			break;
+//		printf("%d\n", i);
+//		i++;
+//	} while (i <= 10);
+//}
+
+//{
+//	int i=0;
+//	int ret = 1;
+//	int n = 0;
+//	scanf_s("%d", &n);
+//		for (i = 1; i <= n; i++)
+//		{
+//			ret *= i;
+//		}
+//		printf("%d\n", ret);
+//        return 0;
+//}
+
